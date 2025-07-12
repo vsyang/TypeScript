f5cb20a85a1ddc419e1d45bda17b6f1bb935713f
vsyang <yang.vanessa85@gmail.com>
2025-07-11 18:09:59 -0600

    updated readme, updated index, updated ts, recompile js

--- README.md
+++ README.md
@@ -1,32 +1,33 @@
 # Overview
 
-{Important! Do not say in this section that this is college assignment. Talk about what you are trying to accomplish as a software engineer to further your learning.}
+This is my TypeScript project where I created a simple drawing app with HTML5 and Canvas. 
 
-{Provide a description of the software that you wrote to demonstrate the TypeScript language.}
+The purpose of writing this software is to learn and familiarize myself with TypeScript which is a more advanced version of JavaScript. 
 
-{Describe your purpose for writing this software.}
-
-{Provide a link to your YouTube demonstration. It should be a 4-5 minute demo of the software running and a walkthrough of the code. Focus should be on sharing what you learned about the language syntax.}
 
 [Software Demo Video](http://youtube.link.goes.here)
 
 # Development Environment
 
 {Describe the tools that you used to develop the software}
+To create this software, I used Visual Studio Code, and saved into my TypeScript GitHub repository. 
 
-{Describe the programming language that you used and any libraries.}
+I installed the TypeScript extension and compiled the ts file into a JavaScript file. 
 
 # Useful Websites
 
 {Make a list of websites that you found helpful in this project}
 
-- [Web Site Name](http://url.link.goes.here)
-- [Web Site Name](http://url.link.goes.here)
+- [TypeScript](http://www.typescriptlang.org/playground)
+- [YouTube](https://www.youtube.com/watch?v=2pZmKW9-I_k&list=PL4cUxeGkcC9gUgr39Q_yD6v-bSyMwKPUI&ab_channel=NetNinja)
+- [Mozilla Developer HTML](https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement)
+- [Mozilla Developer Canvas](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D)
 
 # Future Work
 
-{Make a list of things that you need to fix, improve, and add in the future.}
+In the future, I would like to improve:
 
-- Item 1
-- Item 2
-- Item 3
+- Loading the drawings correctly
+- The style (css) portion. There was not enough time this week.
+- The bug that occasionally occurs when actually drawing. Sometimes, clicking on the mouse causes a starburst of lines instead of the line the user is drawing.
+- Adding an undo/redo feature in case the eraser takes too long
--- dist/app.js
+++ dist/app.js
@@ -1,165 +1,150 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 class DrawingApp {
-    private canvas: HTMLCanvasElement; 
-    private ctx: CanvasRenderingContext2D; 
-    private drawing = false;
-    private color = "#000000";
-    private tool = "pencil";
-    private history: { 
-        x: number, 
-        y: number, 
-        dragging: boolean, 
-        color: string, 
-        tool: string} [] = [];
-
     // constructor to setup for canvas
     constructor() {
-        this.canvas = document.getElementById("drawingCanvas") as HTMLCanvasElement;
-        this.ctx = this.canvas.getContext("2d")!;
+        this.drawing = false;
+        this.color = "#000000";
+        this.tool = "pencil";
+        this.history = [];
+        this.canvas = document.getElementById("drawingCanvas");
+        this.ctx = this.canvas.getContext("2d");
         this.setupListeners();
         console.log("App is ready to go");
     }
-
     //set up listeners for when user clicks, holds down to draw, and save/load
     setupListeners() {
-        const colorPicker = document.getElementById("colorPicker") as HTMLInputElement;
-        const pencilBtn = document.getElementById("pencilBtn")!;
-        const eraserBtn = document.getElementById("eraserBtn")!;
-        const saveBtn = document.getElementById("saveBtn")!;
-        const loadBtn = document.getElementById("loadBtn")!;
-        const drawingSelect = document.getElementById("loadDrawing") as HTMLSelectElement;
-        const drawingName = document.getElementById("inputName") as HTMLInputElement;
-
+        const colorPicker = document.getElementById("colorPicker");
+        const pencilBtn = document.getElementById("pencilBtn");
+        const eraserBtn = document.getElementById("eraserBtn");
+        const saveBtn = document.getElementById("saveBtn");
+        const loadBtn = document.getElementById("loadBtn");
+        const drawingSelect = document.getElementById("loadDrawing");
+        const drawingName = document.getElementById("inputName");
         colorPicker.addEventListener("input", (e) => {
             this.color = colorPicker.value;
             console.log(`Color changed to ${this.color}`);
         });
-
         pencilBtn.addEventListener("click", () => {
             this.tool = "pencil";
             console.log("You have selected the pencil");
         });
-
         eraserBtn.addEventListener("click", () => {
             this.tool = "eraser";
             console.log("You have selected the eraser");
         });
-
         saveBtn.addEventListener("click", () => {
             const name = drawingName.value.trim();
             if (name) {
                 this.saveDrawing(name);
                 this.showList();
                 drawingName.value = "";
-            } else {
-                alert("What would you like to name your drawing?")
+            }
+            else {
+                alert("What would you like to name your drawing?");
             }
         });
-
         loadBtn.addEventListener("click", () => {
             const selected = drawingSelect.value;
             if (selected) {
                 this.loadDrawing(selected);
-            } else {
-                alert("Please select a drawing to load")
+            }
+            else {
+                alert("Please select a drawing to load");
             }
         });
-
         this.canvas.addEventListener("mousedown", (e) => this.startDraw(e));
         this.canvas.addEventListener("mouseup", () => this.stopDraw());
         this.canvas.addEventListener("mousemove", (e) => this.draw(e));
-
         this.showList();
     }
-
     //functions for actual drawing
-
     //when mouse is clicked then held down
-    startDraw(e: MouseEvent) {
+    startDraw(e) {
         this.drawing = true;
         this.addStroke(e, false);
     }
-
     //when mouse button is let go
     stopDraw() {
         this.drawing = false;
         this.ctx.beginPath();
     }
-
     //when mouse is held down and moved
-    draw(e: MouseEvent) {
-        if (!this.drawing) return;
+    draw(e) {
+        if (!this.drawing)
+            return;
         this.addStroke(e, true);
     }
-
-
-    addStroke(e: MouseEvent, dragging: boolean) {
+    addStroke(e, dragging) {
         const x = e.offsetX;
         const y = e.offsetY;
         const currentColor = this.tool === "eraser" ? "#FFFFFF" : this.color;
-
         this.ctx.strokeStyle = currentColor;
         this.ctx.lineWidth = 2;
         this.ctx.lineTo(x, y);
         this.ctx.stroke();
         this.ctx.beginPath();
         this.ctx.moveTo(x, y);
-
-        this.history.push({ x, y, dragging, color: currentColor, tool: this.tool })
+        this.history.push({ x, y, dragging, color: currentColor, tool: this.tool });
     }
-
     //async function to save drawing to local storage
-    async saveDrawing(name: string) {
-        await new Promise((resolve) => setTimeout(resolve, 1000)); //simulate timed delay
-        localStorage.setItem(`Drawing: ${name}`, JSON.stringify(this.history));
-        console.log(`Your drawing "${name}has been saved`);
+    saveDrawing(name) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield new Promise((resolve) => setTimeout(resolve, 1000)); //simulate timed delay
+            localStorage.setItem(`Drawing: ${name}`, JSON.stringify(this.history));
+            console.log(`Your drawing "${name}has been saved`);
+        });
     }
-
     //async function to load drawing from list
-    async loadDrawing(name: string) {
-        await new Promise((resolve) => setTimeout(resolve, 1000)); //simulate timed delay
-        this.clearCanvas();
-        const data = localStorage.getItem(`Drawing: ${name}`);
-        if (!data || data === "[]") {
-            try {
-                throw new Error("That drawing doesn't exist");
-            } catch (err) {
-                const error = err as Error;
-                console.error(error.message);
-                alert(error.message);
-                return;
-            } 
-        }
-
-
-        const strokes = JSON.parse(data);
-        this.replayDrawing(strokes, 0);
-        console.log("Drawing ready to go!");
+    loadDrawing(name) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield new Promise((resolve) => setTimeout(resolve, 1000)); //simulate timed delay
+            this.clearCanvas();
+            const data = localStorage.getItem(`Drawing: ${name}`);
+            if (!data || data === "[]") {
+                try {
+                    throw new Error("That drawing doesn't exist");
+                }
+                catch (err) {
+                    const error = err;
+                    console.error(error.message);
+                    alert(error.message);
+                    return;
+                }
+            }
+            const strokes = JSON.parse(data);
+            this.replayDrawing(strokes, 0);
+            console.log("Drawing ready to go!");
+        });
     }
-
     //reset canvas
     clearCanvas() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.beginPath();
     }
-
-    replayDrawing(strokes: any[], index: number) {
-        if (index >= strokes.length) return;
+    replayDrawing(strokes, index) {
+        if (index >= strokes.length)
+            return;
         const stroke = strokes[index];
-
         this.ctx.beginPath();
         this.ctx.moveTo(stroke.x, stroke.y);
         this.ctx.strokeStyle = stroke.color;
         this.ctx.lineWidth = 2;
         this.ctx.lineTo(stroke.x, stroke.y);
-
         setTimeout(() => this.replayDrawing(strokes, index + 1), 5); //recursive
     }
-
     //function to show list of saved drawings
     showList() {
-        const drawingSelect = document.getElementById("loadDrawing") as HTMLSelectElement;
+        const drawingSelect = document.getElementById("loadDrawing");
         drawingSelect.innerHTML = '<option disabled selected>Select saved drawing</option>';
-
         for (let i = 0; i < localStorage.length; i++) {
             const key = localStorage.key(i);
             if (key && key.startsWith("Drawing:")) {
@@ -172,7 +157,6 @@ class DrawingApp {
         }
     }
 }
-
 window.onload = () => {
     new DrawingApp();
 };
